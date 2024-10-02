fc8b6106856e55d2e8ee2c112cac1a154d469cac
Jose <josesm82@gmail.com>
2024-10-02 22:24:24 +0200

    update service to allow receive list of transactions

--- python/server.py
+++ python/server.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
+
+class Transaction(BaseModel):
+    date: str
+    amount: float
+    name: str
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/")
-def calculate_budget():
-    return {"Hello": "World"}
+@app.post("/calculate_budget")
+def calculate_budget(transactions: List[Transaction]):
+    return {"Message to show to the user based on his expenses"}
 
